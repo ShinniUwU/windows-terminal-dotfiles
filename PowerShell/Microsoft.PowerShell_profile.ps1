030be1b71ec1d2730aba9a58c5c9bf0b25d4c718
Hana <69644134+ShinniUwU@users.noreply.github.com>
2024-05-28 22:57:20 +0300

    fixed an error wasn't loading the config + added comments

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,12 @@
-clear-host
+# Clear the host
+Clear-Host
+
+# Run fastfetch (if installed)
 fastfetch
 
-$ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
+# Set the environment variable for the Starship configuration
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+
+
+# Initialize Starship prompt
 Invoke-Expression (&starship init powershell)
